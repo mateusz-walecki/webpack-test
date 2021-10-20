eead5e116a0e99ec7252a4bdc98c494b763e839c
Mateusz Walęcki <m.walecki1992@o2.pl>
2021-10-20 20:27:55 +0200

    new file: Mateusz

--- webpack.common.js
+++ webpack.common.js
@@ -88,5 +88,11 @@ module.exports = {
       chunks: ["index", "another"],
       filename: "another.html",
     }),
+    new HtmlWebpackPlugin({
+      template: "./src/pages/mateusz.html",
+      inject: true,
+      chunks: ["index",],
+      filename: "mateusz.html",
+    }),
   ],
 };
